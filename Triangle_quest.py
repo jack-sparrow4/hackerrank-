8246198595e67c9b55e1a682ee8f5451e1cfd787
Shubham <shubhamprasad910@gmail.com>
2020-11-26 12:51:24 +0530

    did it again

--- Triangle_quest.py
+++ Triangle_quest.py
@@ -1,3 +1,4 @@
 print("we have done changes")
+print("we have to upload our projects and notebooks on github to show to world")
 for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
     print((((10**i)-1)//9)*i)
